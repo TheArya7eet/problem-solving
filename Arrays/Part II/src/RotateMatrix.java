7c9b5b7ec304f79983e622fe4d403d0c4153dabe
Aryajeet Ghosh <aryajeetghosh1@gmail.com>
2023-08-18 01:33:15 +0530

    RotateMatrix.java file updated

--- Arrays/Part II/src/RotateMatrix.java
+++ Arrays/Part II/src/RotateMatrix.java
@@ -45,7 +45,9 @@ public class RotateMatrix {
     }
 
     public static void main(String[] args) {
-        int[][] matrix = {{1,2,3}, {4,5,6}, {7,8,9}};
+        int[][] matrix = {{1,2,3},
+                          {4,5,6},
+                          {7,8,9}};
 
         rotate(matrix);
 
